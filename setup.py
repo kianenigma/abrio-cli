38d3b9e0c2c91dcc361341744bb9be93ffc5d136
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2016-04-25 18:57:06 +0430

    Adding additional setup info tags and licese

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup
 setup(
     # Main Descriptions
     name="AbrIO",
-    version="0.0.1",
+    version="0.0.3",
+    description = 'Abrio Engine cli for developers',
+    author = 'AbrIO',
+    author_email = 'abrioengine@gmail.com',
+    url = 'https://github.com/Abrioengine/abrio-cli',
+    download_url = 'https://github.com/Abrioengine/abrio-cli/archive/0.0.3.tar.gz',
 
     # Included packages
     packages=['abriocli'],
